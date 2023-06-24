c1ccc7587bf7a1e2e3e43d036aa3c9c2f08c8d20
Adi33e <99587841+Adi33e@users.noreply.github.com>
2023-06-24 23:39:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
+const BotName = 'BOT KONTOL'; 
+const instagram = 'adii_yrnm'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:SAYAANJING\n' // Nama kamu
+            + 'ORG:BOT KONTOL;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6283821308876:+62 838-2130-8876\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
